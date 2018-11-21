3dd88943b48053fedfe8746ac33eecb152cac3da
Luiz Silva <luizjrdeveloper@gmail.com>
2018-11-21 11:50:02 -0200

    Att.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 echo "Instalando RaspiWiFi Headless..."
-git clone https://github.com/marvmex/IoT-RaspiWiFi.git
+git clone https://github.com/luizjrdeveloper/IoT-RaspiWiFi.git
 cd IoT-RaspiWiFi
 echo "Iniciando..."
 sudo sh start.sh
